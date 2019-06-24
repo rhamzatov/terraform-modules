9e036ca0429a0127628f9c5de9fc255e56449281
sgaliamov-albelli <salavat.galiamov@albelli.com>
2019-06-24 13:01:01 +0200

    Feature/cbo 355 adjust outputs (#9)

--- src/modules/patterns/lambda/with_error_alerts/output.tf
+++ src/modules/patterns/lambda/with_error_alerts/output.tf
@@ -1,3 +1,19 @@
-output "app_name" {
-  value = "${var.app_name}"
+output "lambda_arn" {
+  value = "${module.lambda.lambda_arn}"
+}
+
+output "lambda_invoke_arn" {
+  value = "${module.lambda.lambda_invoke_arn}"
+}
+
+output "role_arn" {
+  value = "${module.lambda.role_arn}"
+}
+
+output "role_name" {
+  value = "${module.lambda.role_name}"
+}
+
+output "log_group_name" {
+  value = "${module.lambda.log_group_name}"
 }

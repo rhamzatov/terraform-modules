89303a79afd179e990e9f859043c5f774f12aa12
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2019-12-20 14:52:04 +0100

    rename to s3_write_only and limit to only put object

--- src/modules/resources/iam/s3_write_only/main.tf
+++ src/modules/resources/iam/s3_write_only/main.tf
@@ -1,9 +1,7 @@
 data "aws_iam_policy_document" "app" {
   statement {
     actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:ListBucket"
+      "s3:PutObject"
     ]
 
     resources = ["${var.s3_bucket_arn}"]

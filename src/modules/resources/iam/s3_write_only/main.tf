c8d05ca1df929cc963ac34967a34a7c820cf81c4
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2020-01-13 14:48:04 +0100

    fix s3 permissions

--- src/modules/resources/iam/s3_write_only/main.tf
+++ src/modules/resources/iam/s3_write_only/main.tf
@@ -1,7 +1,8 @@
 data "aws_iam_policy_document" "app" {
   statement {
     actions = [
-      "s3:PutObject"
+      "s3:GetObjectVersion",
+      "s3:PutObject",
     ]
 
     resources = ["${var.s3_bucket_arn}/*"]

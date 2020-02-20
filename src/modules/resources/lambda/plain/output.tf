4b92c464199873903ea7c408ebe558fd2b7b8110
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2020-02-20 15:57:15 +0100

    upgraded test

--- src/modules/resources/lambda/plain/output.tf
+++ src/modules/resources/lambda/plain/output.tf
@@ -1,12 +1,12 @@
 output "arn" {
-  value = aws_lambda_function.app.*.arn
+  value = aws_lambda_function.app.arn
 }
 
 output "qualified_arn" {
-  value = aws_lambda_function.app.*.qualified_arn
+  value = aws_lambda_function.app.qualified_arn
 }
 
 output "invoke_arn" {
-  value = aws_lambda_function.app.*.invoke_arn
+  value = aws_lambda_function.app.invoke_arn
 }
 

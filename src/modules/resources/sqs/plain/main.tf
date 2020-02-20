22bcb57ccc80a8aa1c379c8ca4817bb421df6901
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2020-02-20 15:23:58 +0100

    upgraded sqs plain

--- src/modules/resources/sqs/plain/main.tf
+++ src/modules/resources/sqs/plain/main.tf
@@ -1,11 +1,12 @@
 resource "aws_sqs_queue" "queue" {
-  name                       = "${var.name}"
-  visibility_timeout_seconds = "${var.visibility_timeout_seconds}"
-  message_retention_seconds  = "${var.message_retention_seconds}"
-  max_message_size           = "${var.max_message_size}"
-  delay_seconds              = "${var.delay_seconds}"
-  receive_wait_time_seconds  = "${var.receive_wait_time_seconds}"
-  redrive_policy             = "${var.redrive_policy}"
-  policy                     = "${var.policy}"
-  tags                       = "${var.tags}"
+  name                       = var.name
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  message_retention_seconds  = var.message_retention_seconds
+  max_message_size           = var.max_message_size
+  delay_seconds              = var.delay_seconds
+  receive_wait_time_seconds  = var.receive_wait_time_seconds
+  redrive_policy             = var.redrive_policy
+  policy                     = var.policy
+  tags                       = var.tags
 }
+

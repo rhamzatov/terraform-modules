8c0ca92557b8ff433e2d273cf7bb80abb71b170d
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2020-02-20 15:19:41 +0100

    upgraded s3

--- src/modules/resources/s3/private_bucket/outputs.tf
+++ src/modules/resources/s3/private_bucket/outputs.tf
@@ -1,9 +1,10 @@
 output "name" {
-  value       = "${aws_s3_bucket.app.id}"
+  value       = aws_s3_bucket.app.id
   description = "The name of the bucket."
 }
 
 output "arn" {
-  value       = "${aws_s3_bucket.app.arn}"
+  value       = aws_s3_bucket.app.arn
   description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname"
 }
+

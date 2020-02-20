8967d247dd919c00b2bdd992616b15ac45434e54
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2020-02-20 15:08:56 +0100

    upgraded label

--- src/modules/resources/label/main.tf
+++ src/modules/resources/label/main.tf
@@ -1,18 +1,26 @@
 locals {
-  id = "${join(var.delimiter, compact(concat(list(var.application, var.name, var.environment), var.attributes)))}"
+  id = join(
+    var.delimiter,
+    compact(
+      concat([var.application, var.name, var.environment], var.attributes),
+    ),
+  )
 
-  id_without_env = "${join(var.delimiter, compact(concat(list(var.application, var.name), var.attributes)))}"
+  id_without_env = join(
+    var.delimiter,
+    compact(concat([var.application, var.name], var.attributes)),
+  )
 
   # https://wiki.albelli.net/wiki/Albelli_AWS_Tagging_standards
-  tags = "${
-      merge( 
-        map(
-          "Domain", "${lower(var.domain)}",
-          "Environment", "${lower(var.environment)}",
-          "Cost Center", "${lower(var.cost_center)}",
-          "Application", "${lower(var.application)}",
-          "Team", "${lower(var.team)}"
-        ), var.tags
-      )
-    }"
+  tags = merge(
+    {
+      "Domain"      = lower(var.domain)
+      "Environment" = lower(var.environment)
+      "Cost Center" = lower(var.cost_center)
+      "Application" = lower(var.application)
+      "Team"        = lower(var.team)
+    },
+    var.tags,
+  )
 }
+
--- src/modules/resources/label/outputs.tf
+++ src/modules/resources/label/outputs.tf
@@ -1,14 +1,15 @@
 output "id" {
-  value       = "${local.id}"
+  value       = local.id
   description = "Disambiguated ID"
 }
 
 output "id_without_env" {
-  value       = "${local.id_without_env}"
+  value       = local.id_without_env
   description = "Fixed ID for static names (e.g. lambda name)"
 }
 
 output "tags" {
-  value       = "${local.tags}"
+  value       = local.tags
   description = "Normalized Tag map"
 }
+

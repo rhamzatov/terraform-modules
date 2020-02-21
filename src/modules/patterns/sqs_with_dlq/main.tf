5e3af1a190ff7f3f87285b40e48ef83ef169bf95
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2020-02-21 11:37:42 +0100

    upgraded sqs with dlq

--- src/modules/patterns/sqs_with_dlq/main.tf
+++ src/modules/patterns/sqs_with_dlq/main.tf
@@ -1,8 +1,8 @@
 module "dlq" {
   source                     = "../../resources/sqs/plain"
   name                       = "${var.name}-ERROR"
-  visibility_timeout_seconds = "${var.visibility_timeout_seconds}"
-  tags                       = "${var.tags}"
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  tags                       = var.tags
 
   message_retention_seconds = 1209600 # Max
   max_message_size          = 262144  # Max
@@ -10,25 +10,23 @@ module "dlq" {
 
 module "queue" {
   source                     = "../../resources/sqs/plain"
-  name                       = "${var.name}"
-  visibility_timeout_seconds = "${var.visibility_timeout_seconds}"
-  message_retention_seconds  = "${var.message_retention_seconds}"
-  max_message_size           = "${var.max_message_size}"
-  delay_seconds              = "${var.delay_seconds}"
-  receive_wait_time_seconds  = "${var.receive_wait_time_seconds}"
+  name                       = var.name
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  message_retention_seconds  = var.message_retention_seconds
+  max_message_size           = var.max_message_size
+  delay_seconds              = var.delay_seconds
+  receive_wait_time_seconds  = var.receive_wait_time_seconds
   redrive_policy             = "{\"deadLetterTargetArn\":\"${module.dlq.arn}\",\"maxReceiveCount\":${var.max_receive_count}}"
-  policy                     = "${var.policy}"
-  tags                       = "${var.tags}"
+  policy                     = var.policy
+  tags                       = var.tags
 }
 
 data "aws_iam_policy_document" "queue_policy" {
   statement {
-    principals = [
-      {
-        type        = "AWS"
-        identifiers = ["*"]
-      },
-    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
 
     actions = [
       "sqs:GetQueueUrl",
@@ -38,11 +36,20 @@ data "aws_iam_policy_document" "queue_policy" {
       "sqs:SendMessage",
     ]
 
-    resources = ["${module.queue.arn}"]
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibility in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    resources = [module.queue.arn]
   }
 }
 
 resource "aws_sqs_queue_policy" "queue_policy" {
-  queue_url = "${module.queue.id}"
-  policy    = "${data.aws_iam_policy_document.queue_policy.json}"
+  queue_url = module.queue.id
+  policy    = data.aws_iam_policy_document.queue_policy.json
 }
+

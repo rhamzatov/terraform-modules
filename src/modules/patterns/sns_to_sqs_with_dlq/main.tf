b2be6c9a154e4e70e478e8c98706c89b268f4985
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2020-02-21 11:00:54 +0100

    upgraded sns to sqs with dlq

--- src/modules/patterns/sns_to_sqs_with_dlq/main.tf
+++ src/modules/patterns/sns_to_sqs_with_dlq/main.tf
@@ -1,24 +1,25 @@
 module "queue" {
   source                     = "../sqs_with_dlq"
-  name                       = "${var.name}"
-  visibility_timeout_seconds = "${var.visibility_timeout_seconds}"
-  message_retention_seconds  = "${var.message_retention_seconds}"
-  max_message_size           = "${var.max_message_size}"
-  delay_seconds              = "${var.delay_seconds}"
-  receive_wait_time_seconds  = "${var.receive_wait_time_seconds}"
-  max_receive_count          = "${var.max_receive_count}"
-  policy                     = "${var.policy}"
-  tags                       = "${var.tags}"
+  name                       = var.name
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  message_retention_seconds  = var.message_retention_seconds
+  max_message_size           = var.max_message_size
+  delay_seconds              = var.delay_seconds
+  receive_wait_time_seconds  = var.receive_wait_time_seconds
+  max_receive_count          = var.max_receive_count
+  policy                     = var.policy
+  tags                       = var.tags
 }
 
 module "topic" {
   source = "../../resources/sns/plain"
-  name   = "${var.name}"
-  tags   = "${var.tags}"
+  name   = var.name
+  tags   = var.tags
 }
 
 resource "aws_sns_topic_subscription" "topic_subscription" {
-  topic_arn = "${module.topic.arn}"
+  topic_arn = module.topic.arn
   protocol  = "sqs"
-  endpoint  = "${module.queue.arn}"
+  endpoint  = module.queue.arn
 }
+
--- src/modules/patterns/sns_to_sqs_with_dlq/outputs.tf
+++ src/modules/patterns/sns_to_sqs_with_dlq/outputs.tf
@@ -1,24 +1,25 @@
 output "sns_arn" {
-  value       = "${module.topic.arn}"
+  value       = module.topic.arn
   description = "The ARN of the SNS queue"
 }
 
 output "sqs_arn" {
-  value       = "${module.queue.arn}"
+  value       = module.queue.arn
   description = "The ARN of the SQS queue"
 }
 
 output "sqs_url" {
-  value       = "${module.queue.url}"
+  value       = module.queue.url
   description = "The URL for the created SQS queue."
 }
 
 output "dlq_arn" {
-  value       = "${module.queue.dlq_arn}"
+  value       = module.queue.dlq_arn
   description = "The ARN of the DLQ"
 }
 
 output "dlq_url" {
-  value       = "${module.queue.dlq_url}"
+  value       = module.queue.dlq_url
   description = "The URL for the created DLQ SQS queue."
 }
+

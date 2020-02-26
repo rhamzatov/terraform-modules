d973422c1723c55edca970809cc0a60a99a862d5
Felipe Barbirato <Felipe.Barbirato@albelli.com>
2020-02-26 10:34:24 +0100

    add null values as default

--- src/modules/patterns/lambda/with_error_alerts/variables.tf
+++ src/modules/patterns/lambda/with_error_alerts/variables.tf
@@ -12,17 +12,17 @@ variable "handler" {
 }
 
 variable "filepath" {
-  default     = ""
+  default     = null
   description = "(Optional) The path to the function's deployment package within the local filesystem. If defined, The s3_-prefixed options cannot be used."
 }
 
 variable "s3_bucket_name" {
-  default     = ""
+  default     = null
   description = "(Optional) The S3 bucket location containing the function's deployment package. Conflicts with filename. This bucket must reside in the same AWS region where you are creating the Lambda function."
 }
 
 variable "s3_bucket_path" {
-  default     = ""
+  default     = null
   description = "(Optional) The S3 key of an object containing the function's deployment package. Conflicts with filename."
 }
 
